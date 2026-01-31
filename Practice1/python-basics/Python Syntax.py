5274504229d92d50b9ef997ea709d4ea101c2683
Nurailym <n_saduakas@kbtu.kz>
2026-01-31 11:50:20 +0500

    Add examples of Python syntax and comments

--- Practice1/python-basics/Python Syntax.py
+++ Practice1/python-basics/Python Syntax.py
@@ -1,1 +1,24 @@
+if 5 > 2:
+  print("Five is greater than two!")
+  #Python uses indentation to indicate a block of code.
 
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+#The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
+
+#Variables in Python:
+x = 5
+y = "Hello, World!"
+
+#This is a comment.
+print("Hello, World!")
+#Example
+print("Hello"); print("How are you?"); print("Bye bye!") #not error 
+
+
+#If you want to print multiple words on the same line, you can use the end parameter:
+print("Hello World!", end=" ")
+print("I will print on the same line.")
