81e56ca8aeaab4deeef70b15756f89a259366599
Nurailym <n_saduakas@kbtu.kz>
2026-01-31 12:58:58 +0500

    Update Python_Numbers.py

--- Practice1/python-basics/Python_Numbers.py
+++ Practice1/python-basics/Python_Numbers.py
@@ -1,1 +1,35 @@
+#1
 
+print(type(x))  # <class 'int'>
+print(type(y))  # <class 'float'>
+print(type(z))  # <class 'complex'>
+
+#2Преобразование типов (Type Conversion):
+x = 1       # int
+y = 2.8     # float
+z = 1j      # complex
+
+a = float(x)   # int → float
+b = int(y)     # float → int
+c = complex(x) # int → complex
+# Complex → int/float нельзя.
+
+#3
+import random
+print(random.randrange(1, 10))  # случайное число от 1 до 9
+#4
+z1 = 2 + 3j
+z2 = 1 - 1j
+z_sum = z1 + z2
+z_mul = z1 * z2
+print(z_sum)  # 3+2j
+print(z_mul)  # 5+1j
+
+#5
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
