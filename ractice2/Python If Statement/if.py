04a031a1cb40d58e5be9d44c6266b730b0b9c18d
Nurailym <n_saduakas@kbtu.kz>
2026-02-07 11:06:58 +0500

    Update if.py

--- ractice2/Python If Statement/if.py
+++ ractice2/Python If Statement/if.py
@@ -1,1 +1,29 @@
+#0.0
+# Equals: a == b
+# Not Equals: a != b 
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
 
+#simple exam #0.1
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+#1
+number = 15
+if number > 0:
+  print("The number is positive")
+#2 If statement, without indentation (will raise an error):
+
+a = 33
+b = 200
+if b > a:
+print("b is greater than a") # you will get an error
+
+#3
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+  
