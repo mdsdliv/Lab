dc70e8e0906bae2fdda433f1daab0ee35cac0c8c
Nurailym <n_saduakas@kbtu.kz>
2026-03-28 05:27:31 +0500

    Change database name in config.py to 'postgres'

--- practice_7/config.py
+++ practice_7/config.py
@@ -2,4 +2,4 @@
 host = "localhost"
 user = "postgres"        # твой логин (обычно postgres)
 password = "mdsd"        # твой пароль
-db_name = "PostgreSQL 17" # имя базы данных (нужно создать её в pgAdmin или через SQL)
+db_name = "postgres" # имя базы данных (нужно создать её в pgAdmin или через SQL)
