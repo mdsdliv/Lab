1addb1aab485bf9c94d200350e64286a7ec8bc0c
Nurailym <n_saduakas@kbtu.kz>
2026-03-28 05:25:29 +0500

    Update database credentials in config.py

--- practice_7/config.py
+++ practice_7/config.py
@@ -1,5 +1,5 @@
 # config.py
 host = "localhost"
 user = "postgres"        # твой логин (обычно postgres)
-password = "123"        # твой пароль
-db_name = "phonebook_db" # имя базы данных (нужно создать её в pgAdmin или через SQL)
+password = "mdsd"        # твой пароль
+db_name = "PostgreSQL 17" # имя базы данных (нужно создать её в pgAdmin или через SQL)
